68e838c04240438e0a7c3657a8ec0242da5e5217
Laith Alwahaidi <leithw@gmail.com>
2018-04-05 16:34:10 -0400

    Update TradeIncMessagesInterceptor.java

--- app-core/src/main/java/com/mercury/platform/core/utils/interceptor/TradeIncMessagesInterceptor.java
+++ app-core/src/main/java/com/mercury/platform/core/utils/interceptor/TradeIncMessagesInterceptor.java
@@ -50,6 +50,7 @@ public class TradeIncMessagesInterceptor extends MessageInterceptor {
         public boolean isSuitableFor(String message) {
             return message.contains("Hi, I would like") ||
                     message.contains("Hi, I'd like") ||
+                    message.contains("I'd like") ||
                     (message.contains("wtb") && message.contains("(stash"));
         }
 
